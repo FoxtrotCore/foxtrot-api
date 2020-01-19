5b58724def47baca70729d5d6ec6f835f78f33bb
the-ivo-robotnic <theivorobotnic@gmail.com>
2020-01-19 02:26:50 -0800

    added parser

---
+++ api/line.py
@@ -0,0 +1,71 @@
+class Time:
+    def __init__(self, hrs = 0, min = 0, sec = 0, mil = 0):
+        self.hrs = hrs
+        self.min = min
+        self.sec = sec
+        self.mil = mil
+
+    def pad(self, arg, precision=2):
+        for i in range(1, precision):
+            if(arg < 10**i):
+                pad = ""
+                for j in range(0, (precision - i)): pad += "0"
+                return str(pad + str(arg))
+        return str(arg)
+
+    def __lt__(self, src):
+        if(self.hrs < src.hrs): return True
+        elif(self.min < src.min): return True
+        elif(self.sec < src.sec): return True
+        elif(self.mil < src.mil): return True
+        return False
+
+    def __le__(self, src):
+        if(self.hrs < src.hrs): return True
+        elif(self.hrs == src.hrs):
+            if(self.min < src.min): return True
+            elif(self.min == src.min):
+                if(self.sec < src.sec): return True
+                elif(self.sec == src.sec):
+                    if(self.mil < src.mil): return True
+                    elif(self.mil == src.mil): return True
+        return False
+
+    def __gt__(self, src):
+        if(self.hrs > src.hrs): return True
+        elif(self.min > src.min): return True
+        elif(self.sec > src.sec): return True
+        elif(self.mil > src.mil): return True
+        return False
+
+    def __ge__(self, src):
+        if(self.hrs > src.hrs): return True
+        elif(self.hrs == src.hrs):
+            if(self.min > src.min): return True
+            elif(self.min == src.min):
+                if(self.sec > src.sec): return True
+                elif(self.sec == src.sec):
+                    if(self.mil > src.mil): return True
+                    elif(self.mil == src.mil): return True
+        return False
+
+    def __eq__(self, src): return (self.hrs == src.hrs and self.min == src.min and self.sec == src.sec and self.mil == src.mil)
+    def __str__(self): return str(self.pad(self.hrs) + ":" + self.pad(self.min) + ":" + self.pad(self.sec) + "." + self.pad(self.mil, precision=3))
+
+class Line:
+    def __init__(self,
+                 start = Time(),
+                 end = Time(),
+                 name = "N/A",
+                 text = "N/A"):
+         # if(start > end): raise "Cannot start a line after it ends!"
+         self.start = start
+         self.end = end
+         self.name = name
+         self.text = text
+
+    def is_character(self, name): return (self.name.upper() == name.upper())
+    def has_text(self, text): return (self.text.find(text) != -1)
+
+    def __str__(self): return str("[" + str(self.start) + " - " + str(self.end) + "] <" + self.name + ">: " + self.text)
+    def __str__(self): return str("[" + str(self.start) + " - " + str(self.end) + "] <" + self.name + ">: " + self.text)
--- api/server.py
+++ api/server.py
@@ -7,6 +7,19 @@ from simple_http_server import request_map, HttpError, StaticFile, PathValue, Pa
 HOST = "localhost"
 PORT = 8080
 
+BASE_URL="../transcripts/"
+
+# If all is well, this returns None, else it returns a dictionary
+def validate_arg(arg):
+    # Check int type
+    try: int(arg)
+    except ValueError: return {"code": 406, "message": "episode number must be alphanumeric"}
+
+    # Check ep_num range
+    if(int(arg) < 0 or int(arg) > 95): return {"code": 406, "message": "episode range must be between 0-95"}
+
+    return None
+
 @request_map("/", method="GET")
 def root_handle(): return StaticFile('./index.html', 'utf-8'), {"code": 200, "message": "success!"}
 
@@ -15,12 +28,30 @@ def handle_search_request(ep_num=PathValue(),
                           text=Parameter('text', default=''),
                           episode=Parameter('episode', default=''),
                           character=Parameter('character', default='')):
+    res = validate_arg(ep_num) # Input validation
+    if(res != None): return res
+
     if(text == '' and episode == '' and character == ''): # Deal with an empty request
-        log(Mode.WARN, 'Skipping an epmty serach request!')
+        log(Mode.WARN, 'Skipping an empty serach request!')
         return {"code": 406, "message": "cannot serve empty search request!"}
     else:
         # TODO: Search transcripts
         return {"code": 200, "message": { "text": text, "episode": episode, "character": character }}
+
+@request_map("/script/{ep_num}", method="GET")
+def handle_transcript_request(ep_num=PathValue()):
+    res = validate_arg(ep_num) # Input validation
+    if(res != None): return res
+
+    # Padding
+    if(int(ep_num) < 10): ep_num = "00" + str(ep_num)
+    elif(int(ep_num) < 100): ep_num = "0" + str(ep_num)
+
+    # Construct file name
+    file_path = BASE_URL + "eng_" + ep_num + "_Code_Lyoko.ass"
+
+    # Return result
+    return StaticFile(file_path, "UTF-8")
 
 def handle_close():
     log(Mode.WARN, "Closing the server!")
